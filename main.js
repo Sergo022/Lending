71d9387c442d057b924a4e011c955099148dfe03
Sergo022 <serhiinaumik@gmail.com>
2023-05-27 19:48:13 +0300

    new

--- main.js
+++ main.js
@@ -49,3 +49,14 @@ new Swiper('.swiper-container',{
 });
 
 
+$(document).ready(function(){
+    $('.news_card').first().addClass('active').next().slideToggle(0);
+    $('.news_card').click(function(event){
+         //для того щоб при відкритті другого пункту закривався попередній (заглянути в html)
+        if($('.news_cards').hasClass('one')){
+            $('.news_card').not($(this)).removeClass('active');
+            $('.card_img').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+});
