e20afd6d695054dd457699c1ade2b63f923d9844
Sergo022 <serhiinaumik@gmail.com>
2023-06-01 16:21:17 +0300

    dark

--- main.js
+++ main.js
@@ -96,3 +96,33 @@ function togglePasswordVisibility() {
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+
+
+const lightMode = document.querySelector('.sun');
+const darkMode = document.querySelector('.moon');
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+lightMode.onclick = function(){
+  document.documentElement.setAttribute('data-theme', 'dark');
+  lightMode.style.display = "none";
+  darkMode.style.display = "block";
+  localStorage.setItem('theme', 'dark');
+}
+
+darkMode.onclick = function(){
+  document.documentElement.setAttribute('data-theme', 'light');
+  lightMode.style.display = "block";
+  darkMode.style.display = "none";
+  localStorage.setItem('theme', 'light');
+}
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        lightMode.style.display = "none";
+        darkMode.style.display = "block";
+    }
+}
