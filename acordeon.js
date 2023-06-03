0a149339bf146b09b3e8fede0579bfb8e1591c2f
Sergo022 <serhiinaumik@gmail.com>
2023-06-03 22:34:21 +0300

    login/register

---
+++ acordeon.js
@@ -0,0 +1,12 @@
+//акордеон
+$(document).ready(function(){
+    $('.news_card').first().addClass('active').next().slideToggle(0);
+    $('.news_card').click(function(event){
+         //для того щоб при відкритті другого пункту закривався попередній (заглянути в html)
+        if($('.news_cards').hasClass('one')){
+            $('.news_card').not($(this)).removeClass('active');
+            $('.card_img').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle();
+    });
+});
